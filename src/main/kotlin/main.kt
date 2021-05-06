5ef3b0487fc31777726791a04140e26b50b9db20
ikemura23 <banbara.work@gmail.com>
2021-05-06 23:37:33 +0900

    mainにchapter2メソッドを作成

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,16 @@
 import domain.FullName
 
 fun main() {
-    println("hello world")
+    println("chapter2")
+    chapter2()
+
+}
+
+private fun chapter2() {
+    // 値オブジェクトを作成
     val name = FullName(firstName = "ikemura", lastName = "kazutaka")
     println("${name.firstName} ${name.lastName}")
+
+    // 値オブジェクトは代入できない
+    // name.firstName = ""
 }
