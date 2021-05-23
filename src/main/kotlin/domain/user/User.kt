3a17f449e3676fc862589bea7babdef928047192
ikemura23 <banbara.work@gmail.com>
2021-05-23 16:07:38 +0900

    コメント追加

--- src/main/kotlin/domain/user/User.kt
+++ src/main/kotlin/domain/user/User.kt
@@ -7,6 +7,9 @@ data class User(val id: UserId, var name: UserName) {
 
     /**
      * ユーザー名の変更
+     * 4.3 サービスクラスに持たせたいが、ユーザー名を変更するふるまいはUserクラスにもたせる。
+     * 理由は、変更するための条件があったとき、その条件はUserというドメインの制限だからである。
+     * Valueオブジェクトの制限はサービスではなく、ドメインオブジェクトに持たせるべき
      */
     fun changeUserName(name: UserName) {
         this.name = name
