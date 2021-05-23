8f9c81784cf3666d27cbbeca4491836d39268a0c
ikemura23 <banbara.work@gmail.com>
2021-05-23 16:03:45 +0900

    4.3 ユーザー名変更の処理を追加

--- src/main/kotlin/domain/user/User.kt
+++ src/main/kotlin/domain/user/User.kt
@@ -3,4 +3,12 @@ package domain.user
 /**
  * 2.5.3 ユーザーの値オブジェクト
  */
-data class User(val id: UserId, var name: UserName)
+data class User(val id: UserId, var name: UserName) {
+
+    /**
+     * ユーザー名の変更
+     */
+    fun changeUserName(name: UserName) {
+        this.name = name
+    }
+}
