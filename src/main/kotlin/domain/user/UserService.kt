25ff83c8265c7b3b148ae8eb23d52223c8e00d21
ikemura23 <banbara.work@gmail.com>
2021-05-23 16:30:39 +0900

    UserServiceに仮のexistsメソッドを作成

--- src/main/kotlin/domain/user/UserService.kt
+++ src/main/kotlin/domain/user/UserService.kt
@@ -3,4 +3,12 @@ package domain.user
 /**
  * 4.2.2 Userのドメインサービス
  */
-class UserService
+class UserService {
+
+    /**
+     * 重複を確認する
+     */
+    fun exists(user: User): Boolean {
+        return true
+    }
+}
